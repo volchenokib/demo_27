b2954e900faa61e5b1724ffcdc829147207f6af0
Igor Volchenok <checklogon@gmail.com>
2019-04-02 03:36:27 +0700

    added datepickers

--- src/main.js
+++ src/main.js
@@ -1,11 +1,18 @@
-import Vue from 'vue';
-import App from './App.vue';
-import router from './router';
-import store from './store';
-import Vuetify from 'vuetify';
-import 'vuetify/dist/vuetify.min.css';
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+import Vuetify from "vuetify";
+import "vuetify/dist/vuetify.min.css";
+import ru from "vuetify/es5/locale/ru";
+// import ru from "vuetify/src/locale/ru.ts";
 
-Vue.use(Vuetify);
+Vue.use(Vuetify, {
+	lang: {
+		locales: { ru },
+		current: "ru"
+	}
+});
 
 Vue.config.productionTip = false;
 
@@ -13,4 +20,4 @@ new Vue({
 	router,
 	store,
 	render: h => h(App)
-}).$mount('#app');
+}).$mount("#app");
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,84 +1,123 @@
 <template>
-  <v-container align-center text-xs-center>
-    <v-layout row wrap justify-center>
-      <v-card min-width="560">
-        <v-flex xs12 sm6 md4>
-          <v-menu
-            ref="menu"
-            v-model="menu"
-            :close-on-content-click="false"
-            :nudge-right="40"
-            :return-value.sync="date"
-            lazy
-            transition="scale-transition"
-            offset-y
-            full-width
-            min-width="290px"
-          >
-            <template v-slot:activator="{ on }">
-              <v-text-field
-                v-model="date"
-                label="Picker in menu"
-                prepend-icon="event"
-                readonly
-                v-on="on"
-              ></v-text-field>
-            </template>
-            <v-date-picker v-model="date" no-title scrollable>
-              <v-spacer></v-spacer>
-              <v-btn flat color="primary" @click="menu = false">Cancel</v-btn>
-              <v-btn flat color="primary" @click="$refs.menu.save(date)">OK</v-btn>
-            </v-date-picker>
-          </v-menu>
-        </v-flex>
+  <v-layout row wrap justify-center>
+    <v-card min-width="650" class="pa-2">
+      <v-layout wrap>
+        <!-- picker #1 -->
+        <v-menu
+          ref="menuFrom"
+          v-model="menuFrom"
+          :close-on-content-click="false"
+          :nudge-right="-550"
+          lazy
+          transition="scale-transition"
+          offset-y
+          full-width
+          max-width="290px"
+          min-width="290px"
+        >
+          <template v-slot:activator="{ on }">
+            <v-text-field
+              class="input"
+              v-model="computedDateFrom"
+              label="от"
+              persistent-hint
+              prepend-icon="event"
+              readonly
+              v-on="on"
+            ></v-text-field>
+          </template>
+          <v-date-picker
+            v-model="dateFrom"
+            min="2018-01-01"
+            no-title
+            @input="menuFrom = false"
+            scrollable
+            first-day-of-week="1"
+            locale="ru-RU"
+          ></v-date-picker>
+        </v-menu>
 
         <v-spacer></v-spacer>
 
-        <v-flex xs12 sm6 md4>
-          <v-menu
-            ref="menu"
-            v-model="menu"
-            :close-on-content-click="false"
-            :nudge-right="40"
-            :return-value.sync="date"
-            lazy
-            transition="scale-transition"
-            offset-y
-            full-width
-            min-width="290px"
-          >
-            <template v-slot:activator="{ on }">
-              <v-text-field
-                v-model="date"
-                label="Picker in menu"
-                prepend-icon="event"
-                readonly
-                v-on="on"
-              ></v-text-field>
-            </template>
-            <v-date-picker v-model="date" no-title scrollable>
-              <v-spacer></v-spacer>
-              <v-btn flat color="primary" @click="menu = false">Cancel</v-btn>
-              <v-btn flat color="primary" @click="$refs.menu.save(date)">OK</v-btn>
-            </v-date-picker>
-          </v-menu>
-        </v-flex>
-      </v-card>
-    </v-layout>
-  </v-container>
+        <!-- picker #2 -->
+        <v-menu
+          v-model="menuTo"
+          :close-on-content-click="false"
+          :nudge-right="-550"
+          lazy
+          transition="scale-transition"
+          offset-y
+          full-width
+          max-width="290px"
+          min-width="290px"
+        >
+          <template v-slot:activator="{ on }">
+            <v-text-field
+              v-model="computedDateTo"
+              label="до"
+              persistent-hint
+              prepend-icon="event"
+              readonly
+              v-on="on"
+            ></v-text-field>
+          </template>
+          <v-date-picker
+            v-model="dateTo"
+            no-title
+            @input="menuTo = false"
+            scrollable
+            first-day-of-week="1"
+            locale="ru-RU"
+          ></v-date-picker>
+        </v-menu>
+      </v-layout>
+      <v-btn color="primary" flat @click.prevent="submit">сформировать</v-btn>
+    </v-card>
+  </v-layout>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
 
+<style lang="scss">
+.input {
+  position: relative !important;
+}
+</style>
+
+
+<script>
 export default {
   name: "home",
   data: () => ({
-    date: new Date().toISOString().substr(0, 10),
-    menu: false,
-    modal: false,
-    menu2: false
-  })
+    dateFrom: null,
+    dateTo: null,
+    menuFrom: false,
+    menuTo: false
+  }),
+
+  computed: {
+    computedDateFrom() {
+      if (this.dateFrom) {
+        const [year, month, day] = this.dateFrom.split("-");
+        return `${day}.${month}.${year}`;
+      }
+    },
+    computedDateTo() {
+      if (this.dateTo) {
+        const [year, month, day] = this.dateTo.split("-");
+        return `${day}.${month}.${year}`;
+      }
+    }
+  },
+
+  methods: {
+    submit() {
+      console.log(
+        "dateFrom",
+        this.computedDateFrom,
+        "dateTo",
+        this.computedDateTo
+      );
+    }
+  }
 };
 </script>
