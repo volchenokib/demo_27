6ed2190a2d2b6b39dc1545fd103e41f7d72532b3
Igor <igor.volchenok@econophysica.com>
2019-04-03 13:35:29 +0700

    added validation

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,89 +1,103 @@
 <template>
-  <v-card class="pa-4 my-0 mx-auto" max-width="640" elevation="8">
+  <v-card class="pa-4 my-0 mx-auto" max-width="414" elevation="8">
     <v-layout column justify-center>
-      <v-flex row xs12 sm6 md4>
-        <!-- picker #1 -->
-        <v-menu
-          ref="menuFrom"
-          v-model="menuFrom"
-          :close-on-content-click="false"
-          :nudge-bottom="-60"
-          :nudge-right="-220"
-          lazy
-          transition="scale-transition"
-          offset-y
-          full-width
-          max-width="290px"
-          min-width="290px"
-        >
-          <template v-slot:activator="{ on }">
-            <v-text-field
-              class="input"
-              v-model="computedDateFrom"
-              label="от"
-              persistent-hint
-              prepend-icon="event"
-              readonly
-              v-on="on"
-            ></v-text-field>
-          </template>
+      <v-form v-model="demoForm" ref="form">
+        <v-flex row>
+          <!-- picker #1 -->
+          <v-menu
+            ref="menuFrom"
+            v-model="menuFrom"
+            :close-on-content-click="false"
+            :nudge-bottom="-60"
+            :nudge-right="-520"
+            lazy
+            transition="scale-transition"
+            offset-y
+            full-width
+            max-width="290px"
+            min-width="290px"
+          >
+            <template v-slot:activator="{ on }">
+              <v-text-field
+                class="input"
+                v-model="computedDateFrom"
+                label="от"
+                persistent-hint
+                prepend-icon="event"
+                readonly
+                required
+                :rules="fromDateRules"
+                v-on="on"
+                color="indigo darken-1"
+              ></v-text-field>
+            </template>
 
-          <v-date-picker
-            v-model="dateFrom"
-            min="2018-01-01"
-            :max="today"
-            no-title
-            @input="menuFrom = false"
-            scrollable
-            first-day-of-week="1"
-            locale="ru-RU"
-          ></v-date-picker>
-        </v-menu>
+            <v-date-picker
+              v-model="dateFrom"
+              min="2018-01-01"
+              :max="dateTo || today"
+              no-title
+              @input="menuFrom = false"
+              scrollable
+              first-day-of-week="1"
+              locale="ru-RU"
+              color="indigo darken-1"
+            ></v-date-picker>
+          </v-menu>
 
-        <v-spacer></v-spacer>
-        <!-- picker #2 -->
-        <v-menu
-          class="menu"
-          v-model="menuTo"
-          :close-on-content-click="false"
-          :nudge-bottom="-60"
-          :nudge-right="-220"
-          lazy
-          transition="scale-transition"
-          offset-y
-          full-width
-          max-width="290px"
-          min-width="290px"
-        >
-          <template v-slot:activator="{ on }">
-            <v-text-field
-              v-model="computedDateTo"
-              label="до"
-              persistent-hint
-              prepend-icon="event"
-              readonly
-              v-on="on"
-            ></v-text-field>
-          </template>
+          <v-spacer></v-spacer>
+          <!-- picker #2 -->
+          <v-menu
+            class="menu"
+            v-model="menuTo"
+            :close-on-content-click="false"
+            :nudge-bottom="-60"
+            :nudge-right="-520"
+            lazy
+            transition="scale-transition"
+            offset-y
+            full-width
+            max-width="290px"
+            min-width="290px"
+          >
+            <template v-slot:activator="{ on }">
+              <v-text-field
+                v-model="computedDateTo"
+                label="до"
+                persistent-hint
+                prepend-icon="event"
+                readonly
+                required
+                :rules="toDateRules"
+                v-on="on"
+                color="indigo darken-1"
+              ></v-text-field>
+            </template>
 
-          <v-date-picker
-            v-model="dateTo"
-            min="2018-01-01"
-            :max="today"
-            no-title
-            @input="menuTo = false"
-            scrollable
-            first-day-of-week="1"
-            locale="ru-RU"
-          ></v-date-picker>
-        </v-menu>
-      </v-flex>
+            <v-date-picker
+              v-model="dateTo"
+              :min="dateFrom || '2018-01-01'"
+              :max="today"
+              no-title
+              @input="menuTo = false"
+              scrollable
+              first-day-of-week="1"
+              locale="ru-RU"
+              color="indigo darken-1"
+            ></v-date-picker>
+          </v-menu>
+        </v-flex>
 
-      <!-- radio buttons -->
-      <v-radio-group v-model="type" :mandatory="true">
-        <v-radio label="закупка у единственного поставщика" value="type1" color="indigo darken-1"></v-radio>
-        <v-radio label="электронный аукцион" value="type2" color="indigo darken-1"></v-radio>
-      </v-radio-group>
+        <!-- radio buttons -->
+        <v-radio-group v-model="purchaseType" :mandatory="true">
+          <v-radio
+            label="закупка у единственного поставщика"
+            value="singleProvider"
+            color="indigo darken-1"
+          ></v-radio>
+          <v-radio label="электронный аукцион" value="eAuction" color="indigo darken-1"></v-radio>
+        </v-radio-group>
+      </v-form>
     </v-layout>
 
     <v-switch v-model="localStorage" label="Взять из локального хранилища" color="indigo darken-1"></v-switch>
@@ -112,13 +126,16 @@ import Vue from "vue";
 export default {
   name: "home",
   data: () => ({
+    demoForm: "",
+    fromDateRules: [v => !!v || "Это обязательное поле"],
+    toDateRules: [v => !!v || "Это обязательное поле"],
     dateFrom: null,
     dateTo: null,
     menuFrom: false,
     menuTo: false,
-    type: "type1",
+    purchaseType: "singleProvider",
     localStorage: false,
-    url: "https://78.media.tumblr.com/tumblr_m39nv7PcCU1r326q7o1_500.png",
+    url: "https://78.media.tumblr.com/tumblr_m39nv7PcCU1r326q7o1_500.png"
     // url: "http://producthelp.sdl.com/sdl%20trados%20studio/client_en/sample.xml"
   }),
 
@@ -156,29 +173,38 @@ export default {
     },
 
     downloadWithVueResource() {
-      Vue.http({
-        method: "get",
-        url: this.url,
-        responseType: "arraybuffer"
-      })
-        .then(response => {
-          this.forceFileDownload(response);
+      console.log({
+        dateFrom: this.computedDateFrom,
+        dateTo: this.computedDateTo,
+        purchaseType: this.purchaseType,
+        localStorage: this.localStorage
+      });
+      // this.$refs.form.validate();
+      if (this.$refs.form.validate()) {
+        Vue.http({
+          method: "get",
+          url: this.url,
+          responseType: "arraybuffer"
         })
-        .catch(() => console.log("error occured"));
-    },
-
-    submit() {
-      console.log(
-        "dateFrom",
-        this.computedDateFrom,
-        "dateTo",
-        this.computedDateTo,
-        "type",
-        this.type,
-        "localStorage",
-        this.localStorage
-      );
+          .then(response => {
+            this.forceFileDownload(response);
+          })
+          .catch(() => console.log("error occured"));
+      }
     }
+
+    // submit() {
+    //   console.log(
+    //     "dateFrom",
+    //     this.computedDateFrom,
+    //     "dateTo",
+    //     this.computedDateTo,
+    //     "type",
+    //     this.type,
+    //     "localStorage",
+    //     this.localStorage
+    //   );
+    // }
   }
 };
 </script>
