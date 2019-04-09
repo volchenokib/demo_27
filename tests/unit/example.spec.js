541aa2b21191ad202ac4fc3b06bfa91b0e906ad6
Igor Volchenok <checklogon@gmail.com>
2019-04-10 01:25:27 +0700

    created tests

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -1,7 +1,7 @@
 import Vue from "vue";
 import Vuex from "vuex";
 import Vuetify from "vuetify";
-import { shallowMount } from "@vue/test-utils";
+import { shallowMount, mount, createLocalVue } from "@vue/test-utils";
 import demo1 from "@/components/demo1.vue";
 
 // describe("HelloWorld.vue", () => {
@@ -28,7 +28,13 @@ describe("demo1.vue", () => {
 	});
 
 	it("component is rendered", () => {
-		const wrapper = shallowMount(demo1, { store });
+		const wrapper = mount(demo1, { store });
+		// expect(wrapper.html()).toContain('<span class="count">0</span>');
+		// expect(wrapper.contains("v-container-stub")).toBe(true);
+
+		// let btn = wrapper.find('.btn-edit');
+		// expect(btn.exists()).toBe(true);
+
 		expect(wrapper.contains("div")).toBe(true);
 	});
 });
