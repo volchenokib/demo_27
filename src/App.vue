c0fd8909801fe24b93bff597ef8fe52307698e07
Igor Volchenok <checklogon@gmail.com>
2019-04-10 23:57:53 +0700

    changed headline

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,12 @@
   <v-app>
     <v-toolbar app>
       <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify</span>
-        <span class="font-weight-light">MATERIAL DESIGN</span>
+        <span>Econo</span>
+        <span class="font-weight-light">Physica DESIGN</span>
       </v-toolbar-title>
       <v-spacer></v-spacer>
       <v-btn flat href="https://github.com/vuetifyjs/vuetify/releases/latest" target="_blank">
-        <span class="mr-2">Latest Release</span>
+        <span class="mr-2">Choose Theme</span>
       </v-btn>
     </v-toolbar>
 
