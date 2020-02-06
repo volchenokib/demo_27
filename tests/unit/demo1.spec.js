39918075790f314cff1a7e2e035e1072fef672eb
Igor Volchenok <checklogon@gmail.com>
2020-02-07 01:10:36 +0700

    test: refactoring

--- tests/unit/demo1.spec.js
+++ tests/unit/demo1.spec.js
@@ -1,7 +1,7 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
 import Vuetify from 'vuetify';
-import { shallowMount, mount, createLocalVue } from '@vue/test-utils';
+import { mount, createLocalVue } from '@vue/test-utils';
 import demo1 from '@/components/demo1.vue';
 
 const localVue = createLocalVue();
@@ -32,21 +32,21 @@ store = new Vuex.Store({
 	getters
 });
 
-describe('demo1 component', () => {
+describe('download file pipeline', () => {
 	const wrapper = mount(demo1, { store, localVue });
 	// const vm = wrapper.vm;
 
-	it('renders correctly', () => {
-		expect(wrapper.element).toMatchSnapshot();
-	});
+	// it('renders correctly', () => {
+	// 	expect(wrapper.element).toMatchSnapshot();
+	// });
 
-	it('date input error messages works correctly', () => {
-		wrapper.find('button').trigger('click');
-		const errMsg = wrapper.find('.v-messages__message');
-		expect(errMsg.isVisible()).toBe(true);
-	});
+	// it('date input error messages works correctly', () => {
+	// 	wrapper.find('button').trigger('click');
+	// 	const errMsg = wrapper.find('.v-messages__message');
+	// 	expect(errMsg.isVisible()).toBe(true);
+	// });
 
-	describe('submit button', () => {
+	describe('data capture', () => {
 		const wrapper = mount(demo1, { store, localVue });
 
 		it('exist', () => {
@@ -64,6 +64,31 @@ describe('demo1 component', () => {
 			wrapper.find('button').trigger('click');
 			expect(actions.downloadFile).toHaveBeenCalled();
 			expect(actions.downloadFile).toHaveBeenCalledTimes(1);
+		});
+	});
+
+	describe('sending data to the server', () => {
+		const wrapper = mount(demo1, { store, localVue });
+		it('step 1', () => {
+			expect(wrapper.contains('button')).toBe(true);
+		});
+
+		it('step 2', () => {
+			expect(wrapper.contains('button')).toBe(true);
+		});
+	});
+
+	describe('server response processing', () => {
+		it('step 1', () => {
+			expect(wrapper.contains('button')).toBe(true);
+		});
+
+		it('step 2', () => {
+			expect(wrapper.contains('button')).toBe(true);
+		});
+
+		it('step 3', () => {
+			expect(wrapper.contains('button')).toBe(true);
 		});
 	});
 });
